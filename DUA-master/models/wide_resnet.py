c5db93a0fc2ce5a23f68cb57f4ca61d6e30c700c
qianyu-zhu <qz1086@nyu.edu>
2022-11-06 20:35:43 -0500

    modified wide_resnet.py

--- DUA-master/models/wide_resnet.py
+++ DUA-master/models/wide_resnet.py
@@ -92,3 +92,13 @@ class WideResNet(nn.Module):
         out = F.avg_pool2d(out, 8)
         out = out.view(-1, self.nChannels)
         return self.fc(out)
+
+    def get_embedding(self, x):
+        out = self.conv1(x)
+        out = self.block1(out)
+        out = self.block2(out)
+        out = self.block3(out)
+        out = self.relu(self.bn1(out))
+        out = F.avg_pool2d(out, 8)
+        out = out.view(-1, self.nChannels)
+        return out
